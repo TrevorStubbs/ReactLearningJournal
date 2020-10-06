668d371f802387c73d5c7af701139d9ad7c6073c
Trevor Stubbs <stubbste@gmail.com>
2020-10-06 11:23:00 -0700

    notes

--- Udemy/ModernReactWtihRedux/pics/src/components/SearchBar.js
+++ Udemy/ModernReactWtihRedux/pics/src/components/SearchBar.js
@@ -18,7 +18,7 @@ class SearchBar extends React.Component {
             <input
               type="text"
               value={this.state.term}
-              onChange={(e) => this.setState({ term: e.target.value })} 
+              onChange={(e) => this.setState({ term: e.target.value })}
             />
           </div>
         </form>
