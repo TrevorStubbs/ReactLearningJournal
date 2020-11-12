a98ede3cf44b14398634bbab57f9c9a02edf1153
Trevor Stubbs <stubbste@gmail.com>
2020-11-12 12:13:13 -0800

    seach widget finished

--- Udemy/ModernReactWtihRedux/widgets/src/components/Search.js
+++ Udemy/ModernReactWtihRedux/widgets/src/components/Search.js
@@ -4,10 +4,21 @@ import { queryAllByAltText } from '@testing-library/react';
 
 const Search = () => {
   const [term, setTerm] = useState('Programming');
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
 
+  // ================== Debouncing logic ========================
   useEffect(() => {
-    // Fix 1
+    const timerId = setTimeout(() =>{
+      setDebouncedTerm(term);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
+
+  useEffect(() => {
     const search = async () => {
       const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
         params: {
@@ -15,40 +26,63 @@ const Search = () => {
           list: 'search',
           origin: '*',
           format: 'json',
-          srsearch: term
+          srsearch: debouncedTerm
         }
       });
 
       setResults(data.query.search);
     };
 
-    if(term && !results.length){
-      search();
-    } else {
-      const timeoutId = setTimeout(() => {
-        if (term) {
-          search();
-        }
-      }, 500);
+    search();
+  }, [debouncedTerm]);
+  // ===============================================================  
+
+  // =================== original logic =========================
+  // useEffect(() => {
+  //   // Fix 1
+  //   const search = async () => {
+  //     const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+  //       params: {
+  //         action: 'query',
+  //         list: 'search',
+  //         origin: '*',
+  //         format: 'json',
+  //         srsearch: term
+  //       }
+  //     });
+
+  //     setResults(data.query.search);
+  //   };
+
+  //   if(term && !results.length){
+  //     search();
+  //   } else {
+  //     const timeoutId = setTimeout(() => {
+  //       if (term) {
+  //         search();
+  //       }
+  //     }, 500);
       
-      return () => {
-        clearTimeout(timeoutId);
-      };
-    }
+  //     return () => {
+  //       clearTimeout(timeoutId);
+  //     };
+  //   }
 
 
-    // Fix 2
-    // (async () => {
-    //   await axios.get(';lkj');
-    // })();
+  //   // Fix 2
+  //   // (async () => {
+  //   //   await axios.get(';lkj');
+  //   // })();
 
-    // Fix 3
-    // axios.get(';lkj').then((response) => {
-    //   console.log(response.data);
-    // });
+  //   // Fix 3
+  //   // axios.get(';lkj').then((response) => {
+  //   //   console.log(response.data);
+  //   // });
 
 
-  }, [term]);
+  // }, [term, results.length]);
+
+  // ======================================================================
 
   const renderedResults = results.map((result) => {
     return (
