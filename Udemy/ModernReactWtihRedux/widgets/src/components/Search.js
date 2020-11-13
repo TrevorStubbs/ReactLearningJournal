9b7b2e5e664cb28794e15d68a73357e812ae0745
Trevor Stubbs <stubbste@gmail.com>
2020-11-13 11:55:00 -0800

    dropdown widget

--- Udemy/ModernReactWtihRedux/widgets/src/components/Search.js
+++ Udemy/ModernReactWtihRedux/widgets/src/components/Search.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { queryAllByAltText } from '@testing-library/react';
 
 const Search = () => {
   const [term, setTerm] = useState('Programming');
