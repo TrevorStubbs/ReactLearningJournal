faf645cf0edc4e860b34036f424d844ad803f364
Trevor Stubbs <stubbste@gmail.com>
2020-11-18 10:49:13 -0800

    translate widget finished

--- Udemy/ModernReactWtihRedux/widgets/src/components/Translate.js
+++ Udemy/ModernReactWtihRedux/widgets/src/components/Translate.js
@@ -1,5 +1,8 @@
+//AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM
+
 import React, { useState } from 'react';
 import Dropdown from './Dropdown';
+import Convert from './Convert';
 
 const options = [
   {
@@ -13,6 +16,10 @@ const options = [
   {
     label: 'Hindi',
     value: 'hi'
+  },
+  {
+    label: 'Dutch',
+    value: 'nl'
   }
 ];
 
@@ -34,6 +41,12 @@ const Translate = () => {
         selected={language}
         onSelectedChange={setLanguage}
         options={options}
+      />
+      <hr />
+      <h3 className="ui header">Output</h3>
+      <Convert 
+        text={text}
+        language={language}
       />
     </div>
   );
